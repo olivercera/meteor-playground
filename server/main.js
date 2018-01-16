b27db7b544e091b07ac0d67ba1591724d773e3be
Oliver Cera <olivercera@gmail.com>
2018-01-16 09:12:59 -0500

    get meteor path for file creation

--- server/main.js
+++ server/main.js
@@ -3,3 +3,30 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+var customGetPath = function(){
+
+  var fs = require('fs');
+
+  var meteor_root = Npm.require('fs').realpathSync(process.cwd() + '/../');
+  var application_root = Npm.require('fs').realpathSync(meteor_root + '/../');
+
+  // if running on dev mode
+  if (Npm.require('path').basename(Npm.require('fs').realpathSync(meteor_root + '/../../../')) == '.meteor') {
+    application_root = Npm.require('fs').realpathSync(meteor_root + '/../../../../');
+  }
+
+  return application_root;
+}
+
+Meteor.methods({
+  'createFile': function () {
+    
+
+    console.log(customGetPath());
+
+
+    console.log('create a new file');
+  
+  }
+});
